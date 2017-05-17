b0123e036b762925847acde8d044bbb22c03cf55
YavinLeCretin <yavinlecretin@gmail.com>
2017-05-17 17:02:46 +0200

    almost finished auth page

--- src/components/index_search_page/login.tsx
+++ src/components/index_search_page/login.tsx
@@ -3,55 +3,50 @@ import { connect } from 'react-redux';
 import { authorization } from '../../actions/index';
 
 interface IProps {
-    login?: string;
-    password?: string;
-    searchForSubreddits?: any;
+    authorization?: any;
 }
 
 interface IState {
     mounted: boolean;
+    username: string;
+    password: string;
 
 }
 
 const mapDispatchToProps = {authorization};
 
 
-const mapStateToProps = (state: any): IProps => {
-    return {
-        login: state.login || "",
-        password: state.password || ""
-    };
-};
 
-@(connect(mapStateToProps, mapDispatchToProps) as any)
+@(connect(null, mapDispatchToProps) as any)
 export default class Login extends React.Component<IProps, IState> {
 
     constructor(props: any) {
         super(props);
         this.state = {
             mounted: false,
+            username: "",
+            password: ""
         };
 
-        //this.handleChange = this.handleChange.bind(this);
-        console.log(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.FormSubmit = this.FormSubmit.bind(this);
     }
 
-    public LoginAndAuth(e){
-        console.log(this.props);
-        const config = {
-            username: "",
-            password: "",
-
-        }
+    public FormSubmit(e: any){
+        e.preventDefault();
+        this.setState({[e.target.name]: e.target.value});
     }
 
+    public handleChange(event: any): void {
+        this.setState({[event.target.name]: event.target.value});
+    }
 
     public render() {
         return(<div>
             <h1>Login Page</h1>
-            <form onSubmit={this.LoginAndAuth}>
-                <input type="text" name="Login" />
-                <input type="text" name="password" />
+            <form onSubmit={this.FormSubmit}>
+                <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                <input type="text" name="password" value={this.state.password} onChange={this.handleChange} />
                 <input type="submit" className="btn" value="Submit!" />
             </form>
         </div>);
